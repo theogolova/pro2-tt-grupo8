07a34744b4ef60706ca1b93d1dcfc03648cf8364
Felimaison <142600234+Felimaison@users.noreply.github.com>
2024-04-21 19:39:05 -0300

    creo inserciones de las tablas correspondientes y las FK de productos y comentarios

--- myapp/db/data.sql
+++ myapp/db/data.sql
@@ -27,3 +27,102 @@ createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 deletedAt TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
 );
+
+INSERT INTO Usuarios (usuarios_id, email, contraseña, fecha, dni, foto_perfil)
+VALUES 
+(1, 'pepito@udesa.edu.ar', '123', '1940-05-07', 46123804, '/images/users/pepito.png'),
+(2, 'messi@gmail.com', '456', '2010-10-10', 10294852, '/images/users/messi.png'),
+(3, 'julian@udesa.edu.ar', '789', '2011-11-11', 46223904, '/images/users/julian.png'),
+(4, 'enzo@udesa.edu.ar', '181', '1990-01-01', 9987204, '/images/users/enzo.png'),
+(5, 'gallardo@udesa.edu.ar', '912', '2018-09-12', 4612304, '/images/users/gallardo.png');
+
+INSERT INTO productos (productos_id, nombre_foto, nombre_prod, descripcion_prod)
+VALUES 
+(1, '../images/products/jordan 1.webp', 'Nike Jordan', 'Zapatillas Nike Air Jordan');
+INSERT INTO productos (productos_id, nombre_foto, nombre_prod, descripcion_prod)
+VALUES
+(2, '../images/products/nike SB.webp', 'Nike SB', 'Zapatillas Nike SB');
+INSERT INTO productos (productos_id, nombre_foto, nombre_prod, descripcion_prod)
+VALUES
+(3, '../images/products/nike ari max plus.webp', 'Nike Air Max Plus', 'Zapatillas Nike Air Max Plus');
+INSERT INTO productos (productos_id, nombre_foto, nombre_prod, descripcion_prod)
+VALUES
+(4, '../images/products/nike ari max plus black.webp', 'Nike Air Max Plus Black', 'Zapatillas Nike SB');
+INSERT INTO productos (productos_id, nombre_foto, nombre_prod, descripcion_prod)
+VALUES
+(5, '../images/products/nike ari max plus blue.webp', 'Nike Air Max Plus Blue', 'Zapatillas Nike SB');
+INSERT INTO productos (productos_id, nombre_foto, nombre_prod, descripcion_prod)
+VALUES
+(6, '../images/products/adidas samba.webp', 'Adidas Samba', 'Zapatillas Adidas Samba');
+INSERT INTO productos (productos_id, nombre_foto, nombre_prod, descripcion_prod)
+VALUES
+(7, '../images/products/adidas gazelle.webp', 'Adidas Gazelle', 'Zapatillas Adidas Gazelle');
+INSERT INTO productos (productos_id, nombre_foto, nombre_prod, descripcion_prod)
+VALUES
+(8, '../images/products/on running .webp', 'On Running', 'Zapatillas On Running');
+INSERT INTO productos (productos_id, nombre_foto, nombre_prod, descripcion_prod)
+VALUES
+(9, '../images/products/nike airforce one.webp', 'Nike Airforce 1', 'Zapatillas Nike Airforce One');
+INSERT INTO productos (productos_id, nombre_foto, nombre_prod, descripcion_prod)
+VALUES
+(10, '../images/products/puma palermo.webp', 'Puma Palermo', 'Zapatillas Puma Palermo');
+
+
+INSERT INTO comentarios (productoId, usuarioId, comentario)
+VALUES 
+(1, 1, '¡Estas zapatillas son increíbles!'),
+(1, 2, 'Me encanta el diseño de estas zapatillas.'),
+(1, 3, 'Espectaculares zapatillas!');
+INSERT INTO comentarios (productoId, usuarioId, comentario)
+VALUES 
+(2, 1, "No me gustó mucho su diseño" ),
+(2, 2, " Maravillosas y comodas" ),
+(2, 3, "A pesar del precio y su diseño antiguo, muy buenas!" );
+
+INSERT INTO comentarios (productoId, usuarioId, comentario)
+VALUES 
+(1, 1, '¡Estas zapatillas son increíbles!'),
+(1, 2, 'Me encanta el diseño de estas zapatillas.'),
+(1, 3, 'Espectaculares zapatillas!');
+
+INSERT INTO comentarios (productoId, usuarioId, comentario)
+VALUES 
+(4, 1, 'Bonito diseño'),
+(4, 2, 'Buena calidad'),
+(4, 3, 'Estas zapatillas son muy cómodas');
+
+INSERT INTO comentarios (productoId, usuarioId, comentario)
+VALUES 
+(5, 1, 'Gran elección'),
+(5, 2, 'Me encanta el color'),
+(5, 3, 'Zapatillas muy versátiles');
+
+INSERT INTO comentarios (productoId, usuarioId, comentario)
+VALUES 
+(6, 1, 'Muy cómodas'),
+(6, 2, 'Excelente calidad'),
+(6, 3, 'Estas zapatillas son perfectas para mí');
+
+INSERT INTO comentarios (productoId, usuarioId, comentario)
+VALUES 
+(7, 1, 'Estas zapatillas son clásicas'),
+(7, 2, 'Me gustaría tenerlas en todos los colores'),
+(7, 3, 'Son muy cómodas');
+
+INSERT INTO comentarios (productoId, usuarioId, comentario)
+VALUES 
+(8, 1, 'Buenas zapatillas para correr'),
+(8, 2, 'Excelente amortiguación'),
+(8, 3, 'Me encanta el diseño');
+
+INSERT INTO comentarios (productoId, usuarioId, comentario)
+VALUES 
+(9, 1, 'Estas zapatillas son muy versátiles'),
+(9, 2, 'Perfectas para cualquier ocasión'),
+(9, 3, 'Las recomiendo');
+
+INSERT INTO comentarios (productoId, usuarioId, comentario)
+VALUES 
+(10, 1, 'Muy cómodas'),
+(10, 2, 'Buen diseño'),
+(10, 3, 'Excelente calidad');
