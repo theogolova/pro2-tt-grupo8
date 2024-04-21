71f756cea4d3f7d08a4120e4932c9d4b7511b614
lucasaudouin <laudouin@udesa.edu.ar>
2024-04-21 17:45:55 -0300

    Desarrollo los metodos con sus respectivos Controladores para el proyecto

---
+++ controllers/perfilcontrollers.js
@@ -0,0 +1,40 @@
+const zapatillas = require('../db/data')
+const zapas = require("../db/data")
+const usuario = zapatillas.usuario;
+
+const perfilContoller = {
+    profile: function (req, res, next) {
+        let idUsuario = req.params.id
+        let nombreZapa = [];
+        let descripcionZapa = [];
+        let comentarios = [];
+        let imagenes = [];
+        let id = [];
+        for (let i = 0; i < zapatillas.productos.length; i++) {
+            nombreZapa.push(zapatillas.productos[i].nombre);
+            descripcionZapa.push(zapatillas.productos[i].descripcion);
+            comentarios.push(zapatillas.productos[i].comentarios);
+            imagenes.push(zapatillas.productos[i].imagen);
+            id.push(zapatillas.productos[i].id);
+        }
+        res.render("profile", {
+            title: nombreZapa,
+            descripcion: descripcionZapa,
+            comentarios: comentarios,
+            imagen: imagenes,
+            id: id,
+            usuario: usuario,
+        });
+    },
+    edit: function(req, res, next) {
+        res.render("profile-edit", {title: "Profile Edit", usuario: zapatillas.usuario});
+    },
+    login: function(req, res, next){
+        res.render("login", {title: "Login"});
+    },
+    register: function(req, res, next){
+        res.render("register", {title:"Register"});
+    }
+}; 
+
+module.exports = perfilContoller;
